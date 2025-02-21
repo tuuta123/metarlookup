7252b1e816e1ebbc1c0aa4485d4f25501b93cc47
tuuta123 <150073647+tuuta123@users.noreply.github.com>
2025-02-22 00:00:31 +0200

    Update multiple.js

--- multiple.js
+++ multiple.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Function to fetch and display METAR data
     async function fetchAndDisplayMetar(icao) {
-        const url = `https://aviationweather.gov/api/data/metar?ids=${icao}`;
+        const url = `/weather/${icao}`;
         try {
             const response = await fetch(url);
             if (!response.ok) throw new Error("Error fetching METAR data");
